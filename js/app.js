d3313f0377f51b121dd2a896786966f61424c3a6
Judith <baruki110214@gmail.com>
2021-02-15 12:21:55 -0600

    responsivo

--- js/app.js
+++ js/app.js
@@ -1,12 +1,31 @@
 addEventListener('load', function(){
     new Glider(document.querySelector('.galeria-port'), {
-      slidesToShow: 2.5,
-      slidesToScroll: 2 ,
+      slidesToShow: 1,
+      slidesToScroll: 1 ,
         draggable: true,
-        dots: '.ndicadores',
+        dots: '.indicadores',
         arrows: {
         prev: '.anterior',
         next: '.siguiente'
-        }
+        },
+        responsive: [
+    {
+      // screens greater than >= 775px
+      breakpoint: 400,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },{
+      // screens greater than >= 1024px
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+
+      }
+    }
+  ]
     });
   });
